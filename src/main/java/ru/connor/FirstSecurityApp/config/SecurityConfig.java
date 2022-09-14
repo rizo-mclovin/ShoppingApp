f598a63d84c565613df4b5f99a7acf97250bae23
John Connor <90540757+Xmoon1@users.noreply.github.com>
2022-09-14 20:38:09 +0700

    first security app

---
+++ src/main/java/ru/connor/FirstSecurityApp/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package ru.connor.FirstSecurityApp.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import ru.connor.FirstSecurityApp.security.AuthProvider;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AuthProvider authProvider;
+
+    @Autowired
+    public SecurityConfig(AuthProvider authProvider) {
+        this.authProvider = authProvider;
+    }
+
+    public void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authProvider);
+    }
+}
